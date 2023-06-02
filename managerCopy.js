8f85c5d33a265840cc63b035fd6c971bb82c5ad9
Glaucielle Sá <glauciellesa@gmail.com>
2023-06-02 13:20:41 +0200

    Add json file

--- managerCopy.js
+++ managerCopy.js
@@ -5,27 +5,62 @@ const fs = require("fs");
 const readline = require("readline");
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-let tasks = ["task1", "task2", "task3"];
-
 let welcome =
-  "Welcome to your task manager, Press: \n1. to see all your tasks \n2. to add a task \n3. to delete a task \n4. to mark a task as done \n5. to Exit the task manager\n";
+  "Welcome to your task manager, Press: \n1. to see all your tasks \n2. to add a task \n3. to delete a task \n4. to mark a task as done \n5. to Exit the task manager";
 
-const addTask = () => {
-  return new Promise((resolve) => {
-    rl.question("Which task do you wanna add: ", (task) => {
-      resolve(task);
-      console.log("Your new task: ", task);
-      rl.close();
-    });
+const readTask = () => {
+  fs.readFile("file.json", function (err, data) {
+    if (err) throw err;
+    var array = data.toString().split("\n");
+    for (i in array) {
+      console.log(array[i]);
+    }
   });
 };
 
-// EXEMPLE
+const addTask = () => {
+  rl.question("Which task do you wanna add: ", (task) => {
+    console.log("Your new task: ", task);
+  });
+};
 
-rl.question(welcome, (taskNumber) => {
-  if (taskNumber === "1") {
-    console.log(tasks);
-  } else if (taskNumber === "2") {
-    addTask();
+function replDemo() {
+  return new Promise(function (resolve, reject) {
+    let rl = readline.createInterface(process.stdin, process.stdout);
+    console.log(welcome);
+    rl.setPrompt("task> ");
+    rl.prompt();
+    rl.on("line", function (line) {
+      switch (line.trim()) {
+        case "1":
+          readTask();
+          break;
+        case "2":
+          addTask();
+          break;
+        case "3":
+          addTask();
+          break;
+        case "4":
+          addTask();
+          break;
+        case "5":
+          console.log("bye");
+          rl.close();
+          return;
+      }
+      rl.prompt();
+    });
+  });
+}
+
+async function run() {
+  try {
+    let replResult = await replDemo();
+    console.log("repl result:", replResult);
+  } catch (e) {
+    console.log("failed:", e);
   }
-});
+}
+
+run();
